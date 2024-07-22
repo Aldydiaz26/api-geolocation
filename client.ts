b78f738813961668dc61dc0db21563c33518dcc7
Aldydiaz26 <aldueli24@gmail.com>
2024-07-22 13:39:27 -0300

    add log file to gitignore and update tests in client

--- client.ts
+++ client.ts
@@ -3,16 +3,16 @@ const client = net.createConnection({ port: 3000 });
 
 client.on("connect", () => {
     // Address requests
-    const message = { action: "/address/name", body: { addressName: "Olmos 2010, Laferrere, La Matanza, Argentina" } }
+    const message = { action: "/address/name", body: { addressName: "Av. Juan de Garay 4202, CABA, Argentina" } }
     // const message = { action: "/address/name", body: { addressName: "" } }
     // const message = { action: "/address/name", body: { addressName: null } }
-    // const message = { action: "/address/name", body: {  } }
+    // const message = { action: "/address/name", body: { } }
     // const message = { action: "/address/name" }
-    //  const message = { action:"/address/geometry" , body: {latitude: 37.7937, longitude: -122.3965 } }
-    //  const message = { action:"/address/geometry" , body: {latitude: 37.7937 } }
-    //  const message = { action:"/address/geometry" , body: { longitude: -122.3965 } }
-    //  const message = { action:"/address/geometry" , body: { } }
-    //  const message = { action:"/address/geometry" }
+    // const message = { action:"/address/geometry" , body: { latitude: 37.7937, longitude: -122.3965 } }
+    // const message = { action:"/address/geometry" , body: { latitude: 37.7937 } }
+    // const message = { action:"/address/geometry" , body: { longitude: -122.3965 } }
+    // const message = { action:"/address/geometry" , body: { } }
+    // const message = { action:"/address/geometry" }
 
     // Place requests
     // const message = { action: "/place", body: { latitude: 37.7937, longitude: -122.3965, type: "restaurant" } }
@@ -20,13 +20,13 @@ client.on("connect", () => {
     // const message = { action: "/place", body: { longitude: -122.3965, type: "pizza_restaurant" } }
     // const message = { action: "/place", body: { latitude: 37.7937, type: "shopping_mall" } }
 
+    // Log requests
+    // const message = { action: "/log/all" }
+
     // Incorrect requests
     // const message = {  }
     // const message = { action: "" }
     // const message = { action: "/request" }
-
-    // Log requests
-    // const message = { action: "/log/all" }
 
     const request = JSON.stringify(message)
     client.write(request)
